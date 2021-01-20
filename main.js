156f35ccece046bb3a8a1789fa32cd000bcb9dad
zcc1202 <zc.chew@mail.utoronto.ca>
2021-01-20 17:28:32 -0500

    cleanup

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const Point = require("./point.js");
 const Rectangle = require("./rectangle.js").Rectangle;
 const validateRectangle = require("./rectangle.js").validateRectangle;
-let bottomLeft1 = new Point(4,2);
-let topRight1 = new Point(6,9)
-let rectangleA = new Rectangle(bottomLeft1, topRight1);
 
 function getIntersect(rectangleA, rectangleB) {
     // only works with normalized rectangles
